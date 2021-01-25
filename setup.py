5886aba753793740f899e6670700adce950cd6b9
Håvard Kråkenes <haavardnk@outlook.com>
2021-01-25 15:57:00 +0100

    Add pyyaml, configsuite and npyscreen to requirements

--- setup.py
+++ setup.py
@@ -21,4 +21,5 @@ setup(
     packages=["configsuite_tui"],
     use_scm_version={"write_to": "configsuite_tui/_version.py"},
     setup_requires=["setuptools_scm", "setuptools_scm_about"],
+    install_requires=["pyyaml", "npyscreen", "configsuite"],
 )
