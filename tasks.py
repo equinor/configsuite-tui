3b3bc6af168036a82e33763f718d474bdf98a6fa
Håvard Kråkenes <haavardnk@outlook.com>
2021-01-28 22:19:00 +0100

    Changed testing to use Pyinvoke as psuedo terminal

---
+++ tasks.py
@@ -0,0 +1,23 @@
+from functools import partial
+
+from invoke import task, run
+
+
+sh = partial(run, echo=True)
+
+
+@task
+def lint(c):
+    sh("pylint configsuite_tui")
+
+
+@task
+def check(c):
+    sh("black --check --verbose --diff .")
+    sh("flake8")
+
+
+@task
+def test(c):
+    sh("coverage run --source='.' -m unittest discover", pty=True)
+    sh("coverage report")
--- tests/test_tui.py
+++ tests/test_tui.py
@@ -1,44 +1,23 @@
 import curses
 import sys
-import os
-import select
 from unittest import TestCase
 import npyscreen
-import pyte
 from configsuite_tui.tui import tui
 
 
 class TestTui(TestCase):
     def test_tui_returns_correct_config(self):
-        pid, f_d = os.forkpty()
-        if pid == 0:
-            npyscreen.add_test_input_from_iterable("Joe Biden")
-            npyscreen.TEST_SETTINGS["TEST_INPUT"].append(curses.KEY_DOWN)
-            npyscreen.add_test_input_from_iterable("President")
-            npyscreen.TEST_SETTINGS["TEST_INPUT"].append(curses.KEY_DOWN)
-            npyscreen.add_test_input_from_iterable("78")
-            npyscreen.TEST_SETTINGS["TEST_INPUT"].append(curses.KEY_DOWN)
-            npyscreen.TEST_SETTINGS["TEST_INPUT"].append("^Q")
+        npyscreen.add_test_input_from_iterable("Joe Biden")
+        npyscreen.TEST_SETTINGS["TEST_INPUT"].append(curses.KEY_DOWN)
+        npyscreen.add_test_input_from_iterable("President")
+        npyscreen.TEST_SETTINGS["TEST_INPUT"].append(curses.KEY_DOWN)
+        npyscreen.add_test_input_from_iterable("78")
+        npyscreen.TEST_SETTINGS["TEST_INPUT"].append(curses.KEY_DOWN)
+        npyscreen.TEST_SETTINGS["TEST_INPUT"].append("^Q")
 
-            config = tui(test=True)
-            sys.stdout.write(str(config))
-
-        else:
-            screen = pyte.Screen(60, 24)
-            stream = pyte.ByteStream(screen)
-            while True:
-                try:
-                    [f_d], _, _ = select.select([f_d], [], [], 1)
-                except (KeyboardInterrupt, ValueError):
-                    break
-                else:
-                    try:
-                        data = os.read(f_d, 1024)
-                        stream.feed(data)
-                    except OSError:
-                        break
-
-            self.assertEqual(
-                screen.display[-1],
-                "{'name': 'Joe Biden', 'hobby': 'President', 'age': 78}      ",
-            )
+        config = tui(test=True)
+        sys.stdout.write(str(config))
+        self.assertEqual(
+            config,
+            {"name": "Joe Biden", "hobby": "President", "age": 78},
+        )
