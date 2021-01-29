4352cf26758599515397615583fc07863baed2c0
Håvard Nordhus Kråkenes <haavardnk@outlook.com>
2021-01-29 12:33:39 +0100

    Add tests for load, save, validate and return

--- tests/test_tui.py
+++ tests/test_tui.py
@@ -1,23 +1,96 @@
 import curses
-import sys
+import os
+import tempfile
+import shutil
 from unittest import TestCase
 import npyscreen
 from configsuite_tui.tui import tui
+from configsuite_tui.config import save
 
 
 class TestTui(TestCase):
-    def test_tui_returns_correct_config(self):
-        npyscreen.add_test_input_from_iterable("Joe Biden")
-        npyscreen.TEST_SETTINGS["TEST_INPUT"].append(curses.KEY_DOWN)
-        npyscreen.add_test_input_from_iterable("President")
-        npyscreen.TEST_SETTINGS["TEST_INPUT"].append(curses.KEY_DOWN)
-        npyscreen.add_test_input_from_iterable("78")
-        npyscreen.TEST_SETTINGS["TEST_INPUT"].append(curses.KEY_DOWN)
-        npyscreen.TEST_SETTINGS["TEST_INPUT"].append("^Q")
+    def setUp(self):
+        # Create temporary directory
+        self.tmpdir = tempfile.mkdtemp()
 
-        config = tui(test=True)
-        sys.stdout.write(str(config))
-        self.assertEqual(
-            config,
-            {"name": "Joe Biden", "hobby": "President", "age": 78},
-        )
+        os.chdir(self.tmpdir)
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
+
+    def test_tui_input_save_return_validate(self):
+        with tempfile.NamedTemporaryFile(dir=self.tmpdir) as tmpfile:
+            testinput = [
+                "Jane Doe",
+                curses.KEY_DOWN,
+                "Electrician",
+                curses.KEY_DOWN,
+                "35",
+                curses.KEY_DOWN,
+                "^S",
+                curses.ascii.NL,
+                curses.KEY_RIGHT,
+                curses.ascii.NL,
+                curses.ascii.NL,
+                curses.KEY_DOWN,
+                curses.KEY_DOWN,
+                curses.ascii.NL,
+                "^Q",
+            ]
+            npyscreen.TEST_SETTINGS["TEST_INPUT"] = testinput
+            config, valid = tui(test=True)
+
+            self.assertEqual(
+                config,
+                {"name": "Jane Doe", "hobby": "Electrician", "age": 35},
+            )
+            self.assertTrue(valid)
+            self.assertEqual(
+                tmpfile.read(), b"name: Jane Doe\nhobby: Electrician\nage: 35\n"
+            )
+
+    def test_tui_load_return_validate(self):
+        config = {"name": "John Doe", "hobby": "Carpenter", "age": 45}
+        with tempfile.NamedTemporaryFile(dir=self.tmpdir) as tmpfile:
+            save(config, tmpfile.name)
+            testinput = [
+                curses.KEY_DOWN,
+                curses.KEY_DOWN,
+                curses.KEY_DOWN,
+                "^L",
+                curses.ascii.NL,
+                curses.KEY_RIGHT,
+                curses.ascii.NL,
+                curses.ascii.NL,
+                curses.KEY_DOWN,
+                curses.KEY_DOWN,
+                curses.ascii.NL,
+                "^Q",
+            ]
+            npyscreen.TEST_SETTINGS["TEST_INPUT"] = testinput
+            config, valid = tui(test=True)
+
+            self.assertEqual(
+                config,
+                {"name": "John Doe", "hobby": "Carpenter", "age": 45},
+            )
+            self.assertTrue(valid)
+
+    def test_cancel_forms_and_no_fork(self):
+        testinput = [
+            curses.KEY_DOWN,
+            curses.KEY_DOWN,
+            curses.KEY_DOWN,
+            "^L",
+            curses.KEY_DOWN,
+            curses.ascii.NL,
+            "^S",
+            curses.KEY_DOWN,
+            curses.ascii.NL,
+            "^Q",
+        ]
+        npyscreen.TEST_SETTINGS["TEST_INPUT"] = testinput
+        config, valid = tui()
+
+        self.assertEqual(config, {"age": "", "hobby": "", "name": ""})
+        self.assertFalse(valid)
