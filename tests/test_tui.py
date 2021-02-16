dc50d0ed583fea5d7bddc84609203351a28f0bdc
Håvard Kråkenes <haavardnk@outlook.com>
2021-02-16 21:31:17 +0100

    Add additional schema and tests

--- tests/test_tui.py
+++ tests/test_tui.py
@@ -4,16 +4,19 @@ import os
 import tempfile
 import shutil
 from unittest import TestCase
+import unittest.mock as mock
 import npyscreen
+import pluggy
 from configsuite_tui.tui import tui
 from configsuite_tui.config_tools import save
+from configsuite_tui import hookspecs
+from .schemas import test_schema_2
 
 
 class Test_Tui_With_Files(TestCase):
     def setUp(self):
         # Create temporary directory
         self.tmpdir = tempfile.mkdtemp()
-
         os.chdir(self.tmpdir)
 
     def tearDown(self):
@@ -171,3 +174,82 @@ class Test_Tui_With_Files(TestCase):
             },
         )
         self.assertFalse(valid)
+
+
+class Test_Tui_With_Mocked_Schema(TestCase):
+    pm = pluggy.PluginManager("configsuite_tui")
+    pm.add_hookspecs(hookspecs)
+    pm.load_setuptools_entrypoints("configsuite_tui")
+    pm.register(test_schema_2)
+
+    @mock.patch("configsuite_tui.tui.get_plugin_manager", return_value=pm)
+    def test_load_second_schema(self, mocked_pm):
+
+        testinput = [
+            curses.ascii.NL,
+            curses.ascii.NL,
+            "^Q",
+        ]
+        npyscreen.TEST_SETTINGS["TEST_INPUT"] = testinput
+        config, valid = tui(test=True)
+
+        self.assertEqual(
+            config,
+            {
+                "car": "",
+                "manufacturer": "",
+                "last_serviced": None,
+                "eu_approved": None,
+                "Additional": "",
+            },
+        )
+        self.assertFalse(valid)
+
+    @mock.patch("configsuite_tui.tui.get_plugin_manager", return_value=pm)
+    def test_fill_second_schema(self, mocked_pm):
+        testinput = [
+            curses.ascii.NL,
+            curses.ascii.NL,
+            curses.KEY_DOWN,
+            "Porsche 911",
+            curses.KEY_DOWN,
+            "Porsche",
+            curses.KEY_DOWN,
+            "2021-01-15",
+            curses.KEY_DOWN,
+            curses.ascii.NL,
+            curses.KEY_DOWN,
+            curses.ascii.NL,
+            curses.KEY_DOWN,
+            "Something",
+            curses.KEY_UP,
+            curses.KEY_UP,
+            curses.KEY_UP,
+            curses.KEY_UP,
+            curses.KEY_UP,
+            "^Q",
+        ]
+        npyscreen.TEST_SETTINGS["TEST_INPUT"] = testinput
+        config, valid = tui(test=True)
+
+        self.assertEqual(
+            config,
+            {
+                "car": "Porsche 911",
+                "manufacturer": "Porsche",
+                "last_serviced": datetime.date(2021, 1, 15),
+                "eu_approved": True,
+                "Additional": "Something",
+            },
+        )
+        self.assertTrue(valid)
+
+    @mock.patch("configsuite_tui.tui.get_plugin_manager", return_value=pm)
+    def test_error_blocks_quit(self, mocked_pm):
+        testinput = [
+            "^Q",
+        ]
+        npyscreen.TEST_SETTINGS["TEST_INPUT"] = testinput
+
+        with self.assertRaises(npyscreen.wgwidget.ExhaustedTestInput):
+            tui(test=True)
