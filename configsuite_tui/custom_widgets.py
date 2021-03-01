537ec26dcc0c2f0c099d59becdc9e443f1eefd55
Håvard Kråkenes <haavardnk@outlook.com>
2021-03-01 22:44:58 +0100

    Add custom forms for edit list, load and save

--- configsuite_tui/custom_widgets.py
+++ configsuite_tui/custom_widgets.py
@@ -1,5 +1,5 @@
 import curses
-from npyscreen import FormMultiPage, wgNMenuDisplay
+from npyscreen import FormMultiPage, wgNMenuDisplay, fmActionFormV2, ActionPopup
 
 
 class CustomFormMultiPageWithMenus(FormMultiPage, wgNMenuDisplay.HasMenus):
@@ -23,3 +23,33 @@ class CustomFormMultiPageWithMenus(FormMultiPage, wgNMenuDisplay.HasMenus):
             self.make_attributes_list(footer, curses.A_NORMAL),
             self.columns - x - 1,
         )
+
+
+class CustomEditListPopup(fmActionFormV2.ActionFormV2):
+    DEFAULT_LINES = 12
+    DEFAULT_COLUMNS = 30
+    SHOW_ATX = 10
+    SHOW_ATY = 2
+    OK_BUTTON_TEXT = "Back"
+
+    def create_control_buttons(self):
+        self._add_button(
+            "ok_button",
+            self.__class__.OKBUTTON_TYPE,
+            self.__class__.OK_BUTTON_TEXT,
+            0 - self.__class__.OK_BUTTON_BR_OFFSET[0],
+            0
+            - self.__class__.OK_BUTTON_BR_OFFSET[1]
+            - len(self.__class__.OK_BUTTON_TEXT),
+            None,
+        )
+
+
+class CustomLoadPopup(ActionPopup):
+    OK_BUTTON_TEXT = "Load"
+    CANCEL_BUTTON_TEXT = "Back"
+
+
+class CustomSavePopup(ActionPopup):
+    OK_BUTTON_TEXT = "Save"
+    CANCEL_BUTTON_TEXT = "Back"
