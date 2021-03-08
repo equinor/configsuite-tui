8ed62a9f138ac509360185dac9e440b89473c39c
Håvard Kråkenes <haavardnk@outlook.com>
2021-03-08 23:00:44 +0100

    Update test schemas

--- tests/schemas/test_schema_4.py
+++ tests/schemas/test_schema_4.py
@@ -1,15 +1,6 @@
-import configsuite
 from configsuite import types
 from configsuite import MetaKeys as MK
 import configsuite_tui
-
-
-@configsuite.validator_msg("Is x a string")
-def _is_test(x):
-    return isinstance(x, str)
-
-
-Test_Type = configsuite.BasicType("Test_Type", _is_test)
 
 
 @configsuite_tui.hookimpl
