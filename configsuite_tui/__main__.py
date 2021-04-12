48306f857143dcc4daafc2bd4ef95ad79abd5366
Håvard Kråkenes <haavardnk@outlook.com>
2021-04-12 20:00:29 +0200

    Fix bug: not catching config and schema exceptions from CLI

--- configsuite_tui/__main__.py
+++ configsuite_tui/__main__.py
@@ -5,9 +5,6 @@ from configsuite_tui.tui import tui
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(dest="config", type=str, nargs="?", default="")
-    parser.add_argument(
-        "-c", "--config", type=str, default="", help="Path to config file"
-    )
     parser.add_argument("-s", "--schema", type=str, default="", help="Name of schema")
 
     args = parser.parse_args()
